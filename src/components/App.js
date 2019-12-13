71c98270801cd2912cffe9580ae5cf9a329010d7
benjaminalrt <benjamin.alrt@gmail.com>
2019-12-13 11:48:00 +0100

    2

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/App.css';
+// import Quiz from './Quiz/Quiz';
+// import QuizForm from './Quiz/QuizForm';
+import QuizList from './Quiz/QuizList';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <QuizList />
+      </header>
+    </div>
+  );
+}
+
+export default App;
